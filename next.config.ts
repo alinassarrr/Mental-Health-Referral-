d1e85deea91ad31f19971de55b83f2518a74b645
alinassarrr <aliinassar8@gmail.com>
2026-04-21 02:33:58 +0300

    fix: add maps.google.com to CSP frame-src directive for map embeds

--- next.config.ts
+++ next.config.ts
@@ -14,7 +14,7 @@ const securityHeaders = [
       "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
       "font-src 'self' https://fonts.gstatic.com",
       "img-src 'self' data: https://maps.googleapis.com https://maps.gstatic.com",
-      "frame-src https://www.google.com",                   // Google Maps embeds
+      "frame-src https://www.google.com https://maps.google.com",
       "connect-src 'self' https://docs.google.com",
     ].join('; '),
   },
